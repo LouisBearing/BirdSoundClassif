e0321b22adb7a96cf9a7ad7e29ede47ef079045c
ValBaron10 <valentin.baron10@gmail.com>
2024-04-30 19:09:33 -0500

    Package the src code
    The src is now a package to be used throughout the whole project.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='NBM ML Project',
+    author='Valentin Baron, Matthieu Le Nozach, Hadrien Poinseaux',
+    license='',
+)
